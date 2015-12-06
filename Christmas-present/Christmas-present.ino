8558472bbcadca3ca1165f81cfc0d1cc483d796f
Arthur Timmermans <arthur.timmermans@icloud.com>
2015-12-06 23:14:56 +0100

    pinMode setup

--- Christmas-present/Christmas-present.ino
+++ Christmas-present/Christmas-present.ino
@@ -1,9 +1,13 @@
 void setup() {
-  // put your setup code here, to run once:
-
+  // Pin setup
+  // Green LED: 2,4,6,8,10,12
+  // Red LED: 3,5,7,9,11,13
+   
+  for(int x = 2; x <= 13; x++){
+    pinMode(x,OUTPUT);
+  }
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
 
 }
